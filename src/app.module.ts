9ac205dc378cc83f417274ab8d60b8b6c38d70cb
Bagus Panji <47809071+Baguspanji@users.noreply.github.com>
2025-11-27 17:05:52 +0700

    feat: integrate NestJS ConfigModule and migrate TypeORM to use LibSQL for Turso database connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,11 +10,19 @@ import { Inventory } from './inventories/entities/inventory.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database.sqlite',
-      entities: [Category, Inventory],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: `${configService.get('TURSO_DATABASE_URL')}?authToken=${configService.get('TURSO_AUTH_TOKEN')}`,
+        driver: require('@libsql/sqlite3'),
+        flags: 0x00000040, // SQLITE_OPEN_URI
+        entities: [Category, Inventory],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     CategoriesModule,
     InventoriesModule,
